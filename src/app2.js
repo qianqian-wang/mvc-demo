4820ac3c31bf9e7820dcd6ec5170b64742683fdf
queen <qianqianwang0112@163.com>
2021-09-17 20:21:00 +0800

    修改tab切换bug

--- src/app2.js
+++ src/app2.js
@@ -9,9 +9,7 @@ const m = {
     create() { },
     delete() { },
     update(data) {
-        console.log("zheli4"+data.index)
         Object.assign(m.data, data)
-        console.log("zheli5"+m.data.index)
         eventBus.trigger('m-updated')
     },
     get() { }
@@ -19,19 +17,18 @@ const m = {
 const v = {
     el: null,
     html: (index) => {
-        console.log("zheli7"+index)
-        return `<div class="app">
+        return `
+        <div class="app">
             <div class='app2'>
-                <div class="tab ${index === 0 ? 'selected' : ''}" data-index="0">这个</div>
-                <div class="tab ${index === 1 ? 'selected' : ''}" data-index="1">那个</div>
+                <div class="tab ${parseInt(index) === 0 ? 'selected' : ''}" data-index="0">这个</div>
+                <div class="tab ${parseInt(index) === 1 ? 'selected' : ''}" data-index="1">那个</div>
             </div>
             <div class='content'>
-                <div class="content1 ${index === 0 ? 'active' : ''}">这个显示啦</div>
-                <div class="content1 ${index === 1 ? 'active' : ''}">那个显示啦</div>
+                <div class="content1 ${parseInt(index) === 0 ? 'active' : ''}">这个显示啦</div>
+                <div class="content1 ${parseInt(index) === 1 ? 'active' : ''}">那个显示啦</div>
             </div>
-
-    </div> 
-`},
+        </div> 
+    `},
     init(container) {
         v.el = $(container)  
     },
@@ -39,19 +36,16 @@ const v = {
         if (v.el.children.length !== 0) {
             v.el.empty()
         }
-        console.log("zheli6"+index)
         $(v.html(index)).appendTo(v.el)
     }
 }
 const c = {
-    init(container) {
-        
+    init(container) {      
         v.init(container)
         v.render(m.data.index)
         c.autoBindEvents()
         eventBus.on('m-updated', () => {
             v.render((m.data.index))
-            console.log("zheli1"+m.data.index)
         })
     },
     events: {
@@ -59,8 +53,6 @@ const c = {
     },
     x(e) {
         const index = e.currentTarget.dataset.index
-        console.log("zheli2" + m.data.index)
-        console.log("zheli3"+e.currentTarget.dataset.index)
         m.update({index:index})
     },
     autoBindEvents() {
@@ -76,15 +68,3 @@ const c = {
 }
 
 export default c
-
-// $('.app2').on('click', '', (e) => {
-//     let n = $(e.currentTarget).index()
-//     $('.app2').children().eq(n)
-//         .addClass('selected')
-//         .siblings().removeClass('selected')
-//     $('.content').children()
-//         .eq(n).addClass('active')
-//         .siblings().removeClass('active')
-    
-// })
-// $('.app2').children().eq(0).trigger('click')
